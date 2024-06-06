0d3971bdcb3dbc94249918567408f432ce1e7eb6
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-06-06 08:41:37 +0700

    Add Route Router

--- src/components/Elements/Input/Input.jsx
+++ src/components/Elements/Input/Input.jsx
@@ -1,7 +1,7 @@
 const Input = (props) => {
   const { type, placeHolder, name } = props;
   return (
-    <input type={type} placeholder={placeHolder} className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50" name={name} />
+    <input type={type} placeholder={placeHolder} className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50" name={name} id={name} />
   );
 }
 
