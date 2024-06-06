40cca10fb5c4b95c90831aa9c4b2fa520e899455
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-06-06 10:31:37 +0700

    Add Rendering List

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,22 +1,41 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    image: "/images/shoes-1.jpg",
+    name: "Sepatu gaul",
+    description: `Sepatu ini sangat cocok untuk anda yang ingin tampil beda`,
+    price: "1.000.000"
+  },
+  {
+    id: 2,
+    image: "/images/shoes-2.jpg",
+    name: "Sepatu santai",
+    description: `Sepatu ini sangat cocok untuk anda yang ingin santai`,
+    price: "500.000"
+  },
+  {
+    id: 3,
+    image: "/images/shoes-3.jpg",
+    name: "Sepatu olahraga",
+    description: `Sepatu ini sangat cocok untuk anda yang ingin olahraga`,
+    price: "750.000"
+  }
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu gaul">
-          Sepatu ini sangat cocok untuk anda yang ingin tampil beda
-        </CardProduct.Body>
-        <CardProduct.Footer price="1.000.000"/>
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu keren">
-          Sepatu ini sangat cocok untuk anda yang ingin tampil beda
-        </CardProduct.Body>
-        <CardProduct.Footer price="2.000.000"/>
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price}/>
+        </CardProduct>
+      ))}
     </div>
   );
 }
